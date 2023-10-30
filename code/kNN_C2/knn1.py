085f2c5121a606079bf86522526a39b08696dffa
Saitama <2822863494@qq.com>
2023-10-30 13:40:55 +0800

    numpy overview

---
+++ code/kNN_C2/knn1.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+
+"""
+  @Time    : 2023.10.30 09:42
+  @File    : knn1.py
+  @Project : ml_in_action
+  @Author  : Saitama
+  @IDE     : PyCharm
+  @Desc    : K近邻算法 - knn
+"""
+
